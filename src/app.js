74dbe7e50e1b7003f862c3152d70fb050e2f4e1c
Titah Baudwin <baudwin12@gmail.com>
2024-03-26 14:34:19 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const app = express()
+const user = require("../routes/user")
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
-
+app.use=(user)
 
 
 app.get("/", async(req,res)=>{
