a4c0bed2e8c7feff4101f3d812f5bc57cae78517
Titah Baudwin <baudwin12@gmail.com>
2024-05-17 16:01:02 +0100

    xx

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,6 @@ const {getHome, testCloudinary} = require('../controllers/testController')
 const upload = require('../multer')
 
 router.get("/", getHome)
-router.post("/test-cloud",upload.single("item"))
+// router.post("/test-cloud",upload.single("item"), testCloudinary)
 
 module.exports = router
