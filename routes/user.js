853c4d9f9ac9910970a7dfe47a96aaac542e9436
Titah Baudwin <baudwin12@gmail.com>
2024-05-17 15:59:38 +0100

    xx

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const {getHome, testCloudinary} = require('../controllers/testController')
-// const upload = require('../multer')
+const upload = require('../multer')
 
 router.get("/", getHome)
-router.post("/test-cloud",upload.single("item"), testCloudinary)
+router.post("/test-cloud",upload.single("item"))
 
 module.exports = router
